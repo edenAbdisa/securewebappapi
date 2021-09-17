2abb9bd2a4cd739c73c86683be85b22929bf6334
edenAbdisa <eden.abdisa@yahoo.com>
2021-09-17 10:20:23 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => ['auth:api','scope:user,admin']], function () { 
+    Route::delete('/user/logout', 'UserController@logout');
+    Route::get('/user', 'UserController@index');
+    Route::get('/users', 'UserController@index');
+    Route::post('/user/logout', 'UserController@logout');
+    Route::post('/user/search', 'UserController@search');
+    Route::put('/user/{id}', 'UserController@update');
+
+    Route::get('/feedback', 'FeedbackController@index');
+    Route::post('/feedback', 'FeedbackController@store');
+    Route::put('/feedback/{id}', 'FeedbackController@update');
+    Route::delete('/feedback/{id}', 'FeedbackController@destroy');
+
+    Route::get('/feedbacktype', 'FeedbackTypeController@index');
+    Route::post('/feedbacktype', 'FeedbackTypeController@store');
+    Route::put('/feedbacktype/{id}', 'FeedbackTypeController@update');
+    Route::delete('/feedbacktype/{id}', 'FeedbackTypeController@destroy');
+
+});
+Route::post('/user/login', 'UserController@login');
